0e6b7c04c0864645e6c69d4b91389e9918fa5bed
josephscampo@gmail.com <debian@beaglebone.localdomain>
2018-02-27 00:34:29 +0000

    before IMU failure

--- imutests/mpu6050Test.cpp
+++ imutests/mpu6050Test.cpp
@@ -40,94 +40,115 @@ const char* cmdSet = "i2cset -y 2";
 
 //exec function that runs bash commands in c++
 string exec(char* cmd) {
-	string data;
-	FILE * stream;
-	const int max_buffer = 256;
-	char buffer[max_buffer];
-	strcat(cmd," 2>&1");
-	stream = popen(cmd, "r");
+  string data;
+  FILE * stream;
+  const int max_buffer = 256;
+  char buffer[max_buffer];
+  strcat(cmd," 2>&1");
+  stream = popen(cmd, "r");
 
-	if (stream) {
-		while (!feof(stream))
-			if (fgets(buffer, max_buffer, stream) != NULL) 
-data.append(buffer);
-			pclose(stream);
-		}
-	return data;
+  if (stream) {
+    while (!feof(stream))
+      if (fgets(buffer, max_buffer, stream) != NULL) 
+	data.append(buffer);
+    pclose(stream);
+  }
+  return data;
 }
 
 //function that performs geti2c
 string get(const char* reg1, const char* reg2){
-	char str[100];
-	string str2;
+  char str[100];
+  string str2;
 
-	strcpy(str, cmdGet);
-	strcat(str, reg1);
-	strcat(str, reg2);
+  strcpy(str, cmdGet);
+  strcat(str, reg1);
+  strcat(str, reg2);
 
-	str2 = exec(str);
-	return str2;
+  str2 = exec(str);
+  return str2;
 }
 
 //function that performs seti2c
 void set(const char* reg1, const char* reg2, int value){
-	char str[100];
-	string str2;
+  char str[100];
+  string str2;
 
-	strcpy(str, cmdSet);
-	strcat(str, reg1);
-	strcat(str, reg2);
-	strcat(str, to_string(value).c_str());
+  strcpy(str, cmdSet);
+  strcat(str, reg1);
+  strcat(str, reg2);
+  strcat(str, to_string(value).c_str());
 
 	
-	str2 = exec(str);
+  str2 = exec(str);
 }//CHANGE
 
 int main(){
-	set(deviceADDR, PWR_MGMT_1, 0);	//turn on the MPU6050
-	//+: create export files
-	ofstream out_XAccel,  out_YAccel,  out_ZAccel,
-	  out_XGyro, out_YGyro, out_ZGyro;
-	//+: open export file
-      	out_XAccel.open("Accel_X.txt"); out_YAccel.open("Accel_Y.txt"); out_ZAccel.open("Accel_Z.txt");
-	out_XGyro.open("Gyro_X.txt"); out_YGyro.open("Gyro_Y.txt"); out_ZGyro.open("Gyro_Z.txt");
+
+  
+  set(deviceADDR, PWR_MGMT_1, 0);//turn on the MPU6050
+  system("i2cdump 2 0x68");
+
+  //+:Configure LED
+  std::fstream fs;
+  fs.open ("/sys/kernel/debug/omap_mux/gpmc_ad4"); fs << "7"; fs.close();
+  fs.open("/sys/class/gpio/export"); fs << 67; fs.close();
+  fs.open("/sys/class/gpio/gpio67/direction"); fs << "out"; fs.close();
+        
+  //+: create export files
+  ofstream out_XAccel,  out_YAccel,  out_ZAccel,
+    out_XGyro, out_YGyro, out_ZGyro;
+  out_XAccel.open("Accel_X.txt"); out_YAccel.open("Accel_Y.txt"); out_ZAccel.open("Accel_Z.txt");
+  out_XGyro.open("Gyro_X.txt"); out_YGyro.open("Gyro_Y.txt"); out_ZGyro.open("Gyro_Z.txt");
 	
-	while(true){
-		accel_x = stoi(get(deviceADDR, ACCEL_X_OUT_H), nullptr, 
-16) << 8 + stoi(get(deviceADDR, ACCEL_X_OUT_L), nullptr, 16);
-		accel_y = stoi(get(deviceADDR, ACCEL_Y_OUT_H), nullptr, 
-16) << 8 + stoi(get(deviceADDR, ACCEL_Y_OUT_L), nullptr, 16);
-		accel_z = stoi(get(deviceADDR, ACCEL_Z_OUT_H), nullptr, 
-16) << 8 + stoi(get(deviceADDR, ACCEL_Z_OUT_L), nullptr, 16);
-		accel_x = accel_x;
-		accel_y = accel_y / 16384;
-		accel_z = accel_z / 16384;
+  while(true){
+    fs.open("/sys/class/gpio/gpio67/value"); fs << "0"; fs.close();
+    
+    accel_x = stoi(get(deviceADDR, ACCEL_X_OUT_H), nullptr, 
+		   16) << 8 + stoi(get(deviceADDR, ACCEL_X_OUT_L), nullptr, 16);
+    accel_y = stoi(get(deviceADDR, ACCEL_Y_OUT_H), nullptr, 
+		   16) << 8 + stoi(get(deviceADDR, ACCEL_Y_OUT_L), nullptr, 16);
+    accel_z = stoi(get(deviceADDR, ACCEL_Z_OUT_H), nullptr, 
+		   16) << 8 + stoi(get(deviceADDR, ACCEL_Z_OUT_L), nullptr, 16);
+    accel_x = accel_x / 8192;
+    accel_y = accel_y / 8192;
+    accel_z = accel_z / 8192;
 
-		gyro_x = stoi(get(deviceADDR, GYRO_X_OUT_H), nullptr, 
-16) << 8 + stoi(get(deviceADDR, GYRO_X_OUT_L), nullptr, 16);
-		gyro_y = stoi(get(deviceADDR, GYRO_Y_OUT_H), nullptr, 
-16) << 8 + stoi(get(deviceADDR, GYRO_Y_OUT_L), nullptr, 16);
-		gyro_z = stoi(get(deviceADDR, GYRO_Z_OUT_H), nullptr, 
-16) << 8 + stoi(get(deviceADDR, GYRO_Z_OUT_L), nullptr, 16);
-		gyro_x = gyro_x / 131;
-		gyro_y = gyro_y / 131;
-		gyro_z = gyro_z / 131;
+    //+: LED Control
+    if(accel_x != 0) {
+      cout << "condition is true" << endl;
+      fs.open("/sys/class/gpio/gpio67/value"); fs << "1"; fs.close(); 
+    }
 
 
-		/*cout << "X-acc: " << accel_x << " Y-acc: " << accel_y << 
-" Z-acc: " << accel_z << endl;
-		cout << "X-gyro: " << gyro_x << " Y-gyro: " << gyro_y << 
-		" Z-gyro: " << gyro_z << endl;*/
+    gyro_x = stoi(get(deviceADDR, GYRO_X_OUT_H), nullptr, 
+		  16) << 8 + stoi(get(deviceADDR, GYRO_X_OUT_L), nullptr, 16);
+    gyro_y = stoi(get(deviceADDR, GYRO_Y_OUT_H), nullptr, 
+		  16) << 8 + stoi(get(deviceADDR, GYRO_Y_OUT_L), nullptr, 16);
+    gyro_z = stoi(get(deviceADDR, GYRO_Z_OUT_H), nullptr, 
+		  16) << 8 + stoi(get(deviceADDR, GYRO_Z_OUT_L), nullptr, 16);
+    gyro_x = gyro_x / 66;
+    gyro_y = gyro_y / 66;
+    gyro_z = gyro_z / 66;
+
+    /*cout << "X-acc: " << accel_x << " Y-acc: " << accel_y << 
+      " Z-acc: " << accel_z << endl;
+      cout << "X-gyro: " << gyro_x << " Y-gyro: " << gyro_y << 
+      " Z-gyro: " << gyro_z << endl;*/
 		
-		//+: Write to export file
-		//outputFile << "X-Acc: " << accel_x << endl;
-		out_XAccel << accel_x << endl; out_YAccel << accel_y << endl; out_ZAccel << accel_z << endl;
-		out_XGyro << gyro_x <<endl; out_YGyro << gyro_y <<endl; out_ZGyro << gyro_z <<endl;
-
-	}
-	//+: Close File
-	out_XAccel.close(); out_YAccel.close(); out_ZAccel.close();
-	out_XGyro.close(); out_YGyro.close(); out_ZGyro.close();
-
-	return 0;
+    //+: Write to export file
+    //outputFile << "X-Acc: " << accel_x << endl;
+    out_XAccel << accel_x << endl; out_YAccel << accel_y << endl; out_ZAccel << accel_z << endl;
+    out_XGyro << gyro_x <<endl; out_YGyro << gyro_y <<endl; out_ZGyro << gyro_z <<endl;
+  }
+  
+  /*+: Close File
+  cout << "made it to end" << endl;
+  fs.open("/sys/class/gpio/gpio67/value");
+  fs << "0";
+  fs.close();
+  out_XAccel.close(); out_YAccel.close(); out_ZAccel.close();
+  out_XGyro.close(); out_YGyro.close(); out_ZGyro.close();*/
+	
+  return 0;
 }
