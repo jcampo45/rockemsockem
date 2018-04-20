cd2486748a2cdac60360079575a612a14ff8bc79
Demo User <debian@beaglebone.localdomain>
2018-04-20 07:29:38 +0000

    solenoid double punch

--- rockemMgr/solenoid.cpp
+++ rockemMgr/solenoid.cpp
@@ -3,12 +3,17 @@
 
 int main(int argc, char **argv){
 
-  BlackLib::BlackGPIO solenoid(BlackLib::GPIO_67, BlackLib::output, BlackLib::FastMode);
+  BlackLib::BlackGPIO solenoidR(BlackLib::GPIO_67, BlackLib::output, BlackLib::FastMode);
+  BlackLib::BlackGPIO solenoidL(BlackLib::GPIO_68, BlackLib::output, BlackLib::FastMode);
 
   while(1){
-    solenoid.setValue(BlackLib::high);
+    solenoidL.setValue(BlackLib::high);
     usleep(100000);
-    solenoid.setValue(BlackLib::low);
+    solenoidL.setValue(BlackLib::low);
+    sleep(1);
+    solenoidR.setValue(BlackLib::high);
+    usleep(100000);
+    solenoidR.setValue(BlackLib::low);
     sleep(1);
   }
 }
